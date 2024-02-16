9290e07dd16ac443fdf285f1c15b34c6d92ed777
katsuyama-youhei <K022G0044@g.neec.ac.jp>
2024-02-16 16:51:16 +0900

    02_00まで

---
+++ Object3d.PS.hlsl
@@ -0,0 +1,11 @@
+struct PixelShaderOutput
+{
+    float32_t4 color : SV_Target0;
+};
+
+PixelShaderOutput main()
+{
+    PixelShaderOutput output;
+    output.color = float32_t4(1.0, 1.0, 1.0, 1.0);
+    return output;
+}
