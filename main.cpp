27cf0a0e810c3c1462bdc111daf8c06999fbead5
katsuyama-youhei <K022G0044@g.neec.ac.jp>
2025-01-27 12:51:33 +0900

    ddsファイルの読み込み mipmapを作成するか否か

--- main.cpp
+++ main.cpp
@@ -295,29 +295,45 @@ bool compare(const std::wstring& wstr, const char* cstr) {
 DirectX::ScratchImage LoadTexture(const std::string& filePath) {
 	// テクスチャファイルを読んでプログラムで扱えるようにする
 	DirectX::ScratchImage image{};
-	//std::wstring filePathW = ConvertString(filePath);
+	std::wstring filePathW = ConvertString(filePath);
 
 	// ファイルパス分解用ディレクトリ
 
 	//HRESULT hr = DirectX::LoadFromWICFile(filePathW.c_str(), DirectX::WIC_FLAGS_FORCE_SRGB, nullptr, image);
 
-	HRESULT hr;
-	const char* cstr = "dds";
+	//HRESULT hr;
+	//const char* cstr = "dds";
 	LoadWICTextureFromFile(filePath);
+	
+	//DirectX::ScratchImage image{};
+	//std::wstring filePathW = directoryPath_ + fileName_ + L"."+fileExt_;
 
-	std::wstring filePathW = directoryPath_ + fileName_ + L"."+fileExt_;
+	HRESULT hr;
+	if (filePathW.ends_with(L".dds")) {//DDS
+		hr = DirectX::LoadFromDDSFile(filePathW.c_str(), DirectX::DDS_FLAGS_NONE, nullptr, image);
+	}
+	else {//その他
+		hr = DirectX::LoadFromWICFile(filePathW.c_str(), DirectX::WIC_FLAGS_FORCE_SRGB, nullptr, image);
+	}
+	assert(SUCCEEDED(hr));
 
-	if (compare(fileExt_, cstr)) {
+	/*if (compare(fileExt_, cstr)) {
 		hr = DirectX::LoadFromDDSFile(filePathW.c_str(), DirectX::DDS_FLAGS_NONE, &metadata_, scratchImage_);
 	}
 	else {
 		hr = DirectX::LoadFromWICFile(filePathW.c_str(), DirectX::WIC_FLAGS_FORCE_SRGB, &metadata_, scratchImage_);
 	}
-	assert(SUCCEEDED(hr));
+	assert(SUCCEEDED(hr));*/
 
 	// ミップマップの作成
 	DirectX::ScratchImage mipImages{};
-	hr = DirectX::GenerateMipMaps(scratchImage_.GetImages(), scratchImage_.GetImageCount(), scratchImage_.GetMetadata(), DirectX::TEX_FILTER_SRGB, 0, mipImages);
+	if (DirectX::IsCompressed(image.GetMetadata().format)) {
+		mipImages = std::move(image);//圧縮フォーマットならそのまま利用するのでmoveする
+	}
+	else {
+		hr = DirectX::GenerateMipMaps(image.GetImages(), image.GetImageCount(), image.GetMetadata(), DirectX::TEX_FILTER_SRGB, 0, mipImages);
+	}
+	//hr = DirectX::GenerateMipMaps(scratchImage_.GetImages(), scratchImage_.GetImageCount(), scratchImage_.GetMetadata(), DirectX::TEX_FILTER_SRGB, 0, mipImages);
 	assert(SUCCEEDED(hr));
 
 	// ミップマップ付きのデータを返す
